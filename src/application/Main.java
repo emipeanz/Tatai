b102ebc3250c30e6533b6e643b1fa8091710ca19
maddiebeagley <beagleymaddie@gmail.com>
2017-09-06 16:19:45 +1200

    commit

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,9 +2,11 @@ package application;
 	
 import java.util.concurrent.TimeUnit;
 
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -17,11 +19,19 @@ public class Main extends Application {
 			BorderPane root = new BorderPane();
 			
 			Scene introScene = new Scene(FXMLLoader.load(getClass().getResource("IntroMenu.fxml")));
+			Scene mainScene = new Scene(FXMLLoader.load(getClass().getResource("MainMenu.fxml")));
 			
 			introScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
 			primaryStage.setScene(introScene);
 			primaryStage.setResizable(false);
+			primaryStage.setHeight(400);
+			primaryStage.setWidth(600);
 			primaryStage.show();
+			
+			PauseTransition delay = new PauseTransition(Duration.seconds(2));
+			delay.setOnFinished( event -> primaryStage.setScene(mainScene));
+			delay.play();
 
 		} catch(Exception e) {
 			e.printStackTrace();
