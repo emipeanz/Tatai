568573a0f373ef11ae7e3d4b08a4b7b7f459423f
maddiebeagley <beagleymaddie@gmail.com>
2017-09-07 11:35:19 +1200

    altered functionality of level controller class

--- src/application/LevelController.java
+++ src/application/LevelController.java
@@ -8,7 +8,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 
 public class LevelController {
-	
+
 	@FXML
 	private Label numberToTest;
 	@FXML
@@ -17,15 +17,19 @@ public class LevelController {
 	private ProgressBar progressBar;
 	@FXML
 	private Label progressLabel;
-	
-	private double progress;
+
+	private double progress = 0;
 	//will store all the data associated with the current level
 	private Result _currentLevelResult;
 	//will store all data associated with entire test
 	private Test _test;
-	
-	
-	
+
+
+	public LevelController() {
+		//need to figure out how NOT to hard code this...
+		_test = new Test(Difficulty.HARD);
+	}
+
 	/**
 	 * For now just having a play around - this method is called when the make random number
 	 * button is clicked and will show the number and the word of that number in maori.
@@ -33,29 +37,27 @@ public class LevelController {
 	 * @param event
 	 */
 	public void updateLabels(ActionEvent event) {
-		//need to figure out a way to instantiate this in a constructor cause do not 
-		//want this reinstantiated each time button is pressed.
-		_test = new Test(Difficulty.HARD);
 		
-		_currentLevelResult = new Result(_test._difficulty);
-	
+		_currentLevelResult = new Result(_test._difficulty);	
+
+
 		//sets labels that show a number and the maori word corresponding to it
 		numberToTest.setText(Integer.toString(_currentLevelResult._numberInt));
 		numberWord.setText(_currentLevelResult._numberWord);
 	}
-	
+
 	public void takeRecording() {
 		//part 2 of cbf using bash as do not want to work on VM
 		System.out.println("In method for taking a recording");
-		
+
 		//to be uncommented when there is a valid file for a recording.
-		
+
 		/*
 		File recording = new File("recording");
 		_currentLevelResult.setRecording(recording);
-		*/
+		 */
 	}
-	
+
 	public void hearRecording() {
 		//if recording has been set for a level...
 		if (_currentLevelResult._recording != null) {
@@ -71,6 +73,9 @@ public class LevelController {
 	 * progress bar.
 	 */
 	public void nextLevel() {
+		//stores result of previous test in test model
+		_test.addTestResult(_currentLevelResult);
+		//instantiates a new result for the next level of the test
 		_currentLevelResult = new Result(_test._difficulty);
 		progress += 0.1;
 		progressLabel.setText("Round " + Math.round(progress * 10) + "/10");
