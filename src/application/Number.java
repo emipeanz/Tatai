e9ed6a5657fdb25b60f5cc54e2052c8cee01fbfc
maddiebeagley <beagleymaddie@gmail.com>
2017-09-06 13:35:13 +1200

    Number class made

---
+++ src/application/Number.java
@@ -0,0 +1,13 @@
+package application;
+
+public class Number {
+	
+	public int _numberInt;
+	public String _numberWord;
+	
+	public Number(int number) {
+		_numberInt = number;
+		_numberWord = "tahi";
+	}
+
+}
