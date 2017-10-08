73eb24719f7117df8c5e040b96dd686b3f613e07
maddiebeagley <beagleymaddie@gmail.com>
2017-10-09 12:16:24 +1300

    intermediate menu implemented, functionality works for entering a practice test but not for an equation.

---
+++ src/Controller/DifficultyController.java
@@ -0,0 +1,143 @@
+package Controller;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import Model.Difficulty;
+import Model.TestType;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class DifficultyController {
+
+	@FXML private Button easyButton;
+	@FXML private Button hardButton;
+	@FXML private Button customButton;
+	@FXML private Label hardText;
+	@FXML private Label hardLockSymbol = new Label();
+
+	private TestType testType;
+	private Difficulty difficulty;
+	private boolean unlocked = false;
+
+	public DifficultyController(TestType type) {
+		testType = type;
+	}
+
+	/**
+	 * Takes note of current high score, and sets boolean "unlocked" to true
+	 * if score is above 8. Hard level will only be accessible when unlocked has 
+	 * been set to true.
+	 */
+	public void initialize() {
+		if (testType == testType.PRACTICE) {
+			customButton.setVisible(false);
+		}
+
+		hardText.setVisible(false);
+
+		try {
+			//checks the current highscore
+			String highScoreString = Files.readAllLines(Paths.get(".results.txt")).get(1);
+			int highScore = Integer.parseInt(highScoreString);
+
+			//hard level will only be accessible when user has got 8 questions in a round
+			if (highScore >= 8) {
+				unlocked = true;
+				hardLockSymbol.setText("\uf09c");
+			} 
+		} catch(IOException e) {
+		}
+	}
+
+
+	/**
+	 * This method directs the user to the easy test screen.  A custom controller is made with
+	 * the difficulty parameter and attached to the Level scene
+	 * @param e ActionEvent when easyButton is clicked
+	 */
+	public void enterTest(ActionEvent e) {
+		if ((e.getSource().equals(hardButton) && unlocked) || (e.getSource().equals(easyButton))) {
+
+			// Get the main stage to display the scene in
+			Stage stageEventBelongsTo = (Stage) ((Node)e.getSource()).getScene().getWindow();
+			AnchorPane easyScene = null;
+
+			if (e.getSource().equals(easyButton)) {
+				difficulty = Difficulty.EASY;
+			} else if (e.getSource().equals(hardButton)) {
+				difficulty = Difficulty.HARD;
+			}
+
+			System.out.println(testType);
+			System.out.println(difficulty);
+
+			try {
+				System.out.println("easy set");
+				LevelController controller = new LevelController(difficulty, testType);
+				FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/Level.fxml"));
+				loader.setController(controller);
+				easyScene = loader.load();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			Scene scene = new Scene(easyScene);
+			stageEventBelongsTo.setScene(scene);
+		}
+
+	}
+
+
+	public void enterCustom(ActionEvent e) {
+		System.out.println("Enter custom view");
+	}
+
+	/**
+	 * Method takes an action event on the return to menu button.  The main menu scene is
+	 * displayed and the level view is taken away
+	 * @param event
+	 */
+	public void returnMainMenu(ActionEvent event) {
+		System.out.println("Event triggering return to main menu");
+
+		// Get the main stage to display the scene in
+		Stage stageEventBelongsTo = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		Scene mainMenuScene = null;
+
+		try {
+			mainMenuScene = new Scene(FXMLLoader.load(getClass().getResource("/View/MainMenu.fxml")));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		stageEventBelongsTo.setScene(mainMenuScene);
+	}
+
+	/**
+	 * Displays the text telling user that they need 8+ in a round to progress to hard level.
+	 */
+	public void showHardText() {
+		if (!unlocked) { 
+			hardText.setVisible(true);
+		}
+	}
+
+	/**
+	 * Hides text telling user that they need 8+ in a round to progress to hard level.
+	 */
+	public void hideHardText() {
+		if (!unlocked) {
+			hardText.setVisible(false);
+		}
+	}
+
+
+
+}
--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -6,8 +6,6 @@ package Controller;
  * @author Maddie Beagley and Emilie Pearce
  */
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import Model.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -15,7 +13,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -25,105 +22,40 @@ import javafx.stage.Stage;
  */
 public class MainController{
 
-	@FXML private LevelController levelController;
-	@FXML private Button hardButton = new Button();
-	@FXML private Label hardText = new Label();
-	@FXML private Button easyButton;
-	@FXML private Label hardLockSymbol = new Label();
+	@FXML private Button practiceButton;
+	@FXML private Button playButton;
 	
-	private Difficulty _difficulty;
-	private boolean unlocked = false;
-
-	/**
-	 * Takes note of current high score, and sets boolean "unlocked" to true
-	 * if score is above 8. Hard level will only be accessible when unlocked has 
-	 * been set to true.
-	 */
-	public void initialize() {
-		hardText.setVisible(false);
-
-		try {
-			//checks the current highscore
-			String highScoreString = Files.readAllLines(Paths.get(".results.txt")).get(1);
-			int highScore = Integer.parseInt(highScoreString);
-
-			//hard level will only be accessible when user has got 8 questions in a round
-			if (highScore >= 8) {
-				unlocked = true;
-				hardLockSymbol.setText("\uf09c");
-			} 
-		} catch(IOException e) {
-
-		}
-	}
+	private TestType testType;
 
 	/**
 	 * This method directs the user to the easy test screen.  A custom controller is made with
 	 * the difficulty parameter and attached to the Level scene
 	 * @param e ActionEvent when easyButton is clicked
 	 */
-	public void easyButtonEvent(ActionEvent e) {
+	public void enterDifficultyOption(ActionEvent e) {
 		// Get the main stage to display the scene in
 		Stage stageEventBelongsTo = (Stage) ((Node)e.getSource()).getScene().getWindow();
-
-		AnchorPane easyScene = null;
+		AnchorPane difficultyScene = null;
 		
-		
-		/**
-		 * ARBITRARILY SETTING ENUM AS PRACTICE FOR SAKE OF TESTING 
-		 */
-		
-		TestType type = TestType.PRACTICE;
+		if (e.getSource().equals(practiceButton)) {
+			testType = TestType.PRACTICE;
+		} else if (e.getSource().equals(playButton)) {
+			testType = TestType.EQUATION;
+		}
 		
 		try {
-			System.out.println("easy set");
-			_difficulty = Difficulty.EASY;
-			
-			//ARBITRARILY SET VALUE FOR PRACTICE AS INPUT ENUM
-			LevelController controller = new LevelController(_difficulty, type);
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/Level.fxml"));
+			DifficultyController controller = new DifficultyController(testType);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/Difficulty.fxml"));
 			loader.setController(controller);
-			easyScene = loader.load();
+			difficultyScene = loader.load();
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
-		Scene scene = new Scene(easyScene);
+		Scene scene = new Scene(difficultyScene);
 		stageEventBelongsTo.setScene(scene);
 
 	}
 
-	/**
-	 * This method directs the user to the hard test screen.  A custom controller is made with
-	 * the difficulty parameter and attached to the Level scene
-	 * @param e ActionEvent when hardButton is clicked
-	 */
-	public void hardButtonEvent(ActionEvent e) {
-		if (unlocked) {
-			// Get the main stage to display the scene in
-			Stage stageEventBelongsTo = (Stage) ((Node)e.getSource()).getScene().getWindow();
-
-			AnchorPane hardScene = null;
-			
-			/**
-			 * ARBITRARILY SETTING ENUM AS PRACTICE FOR SAKE OF TESTING 
-			 */
-			
-			TestType type = TestType.PRACTICE;
-			
-			try {
-				System.out.println("hard set");
-				_difficulty = Difficulty.HARD;
-				LevelController controller = new LevelController(_difficulty, type);
-				FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/Level.fxml"));
-				loader.setController(controller);
-				hardScene = loader.load();
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-			Scene scene = new Scene(hardScene);
-			stageEventBelongsTo.setScene(scene);
-		}
-	}
 
 	public void enterStatsView(ActionEvent e) {
 		// Get the main stage to display the scene in
@@ -141,24 +73,6 @@ public class MainController{
 		}
 		Scene scene = new Scene(statsScene);
 		stageEventBelongsTo.setScene(scene);
-	}
-
-	/**
-	 * Displays the text telling user that they need 8+ in a round to progress to hard level.
-	 */
-	public void showHardText() {
-		if (!unlocked) { 
-			hardText.setVisible(true);
-		}
-	}
-
-	/**
-	 * Hides text telling user that they need 8+ in a round to progress to hard level.
-	 */
-	public void hideHardText() {
-		if (!unlocked) {
-			hardText.setVisible(false);
-		}
 	}
 
 }
