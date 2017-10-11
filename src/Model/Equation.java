c3dec0140345057f8d962b74e7cb7bb5e92480eb
maddiebeagley <beagleymaddie@gmail.com>
2017-10-11 19:11:53 +1300

    easy constructor added

--- src/Model/Equation.java
+++ src/Model/Equation.java
@@ -25,28 +25,7 @@ public class Equation extends Question {
 		} else if (difficulty == difficulty.HARD) {
 			hardEquation();
 		} else {
-
-			while (!((answerInt>0) && (answerInt<max+1))) {
-				if (difficulty == difficulty.HARD) {
-					equationOperator = operators[randomNumber(3) - 1];
-				} else {
-					equationOperator = operators[randomNumber(2) - 1];
-				}
-				System.out.println("EQUATION-----------------------------------------------------");
-				System.out.println("operator = " + equationOperator);
-				equationString = randomNumber(max) + equationOperator + randomNumber(max);
-				System.out.println("equation string = " + equationString);
-
-				//evaluates equation
-				try {
-					ScriptEngineManager mgr = new ScriptEngineManager();
-					ScriptEngine engine = mgr.getEngineByName("JavaScript");
-					answerInt = (int) engine.eval(equationString);
-					System.out.println("answer int = " + answerInt);
-
-				} catch (ScriptException e) {
-				}
-			}
+			easyEquation();
 		}
 
 		//stores answer as a string
@@ -54,6 +33,22 @@ public class Equation extends Question {
 		//stores equation as a string to display - doesn't have * 
 		displayString = equationString.replaceAll("\\*", "x");
 	}
+
+	public void easyEquation() {
+		//choosing between all possible operators
+		String operator = operators[randomNumber(2) - 1];
+		int left, right;
+
+		while (!((answerInt >= 10) && (answerInt < 100))) {
+			left = randomNumber(10);
+			right = randomNumber(10);
+			
+			equationString = left + operator + right;
+			System.out.println("equation = " + equationString);
+			answerInt = evaluateEquation(equationString);
+		}
+	}
+
 
 	public void mediumEquation() {
 		//choosing between all possible operators
@@ -74,7 +69,7 @@ public class Equation extends Question {
 			answerInt = evaluateEquation(equationString);
 		}
 	}
-	
+
 	public void hardEquation() {
 		//choosing between all possible operators
 		String operator = operators[randomNumber(3) - 1];
@@ -93,11 +88,11 @@ public class Equation extends Question {
 			answerInt = evaluateEquation(equationString);
 		}
 	}
-	
+
 	public int evaluateEquation(String equation) {
 		//evaluates equation
 		int answer = 0;
-		
+
 		try {
 			ScriptEngineManager mgr = new ScriptEngineManager();
 			ScriptEngine engine = mgr.getEngineByName("JavaScript");
@@ -106,7 +101,7 @@ public class Equation extends Question {
 
 		} catch (ScriptException e) {
 		}
-		
+
 		return answer;
 	}
 
